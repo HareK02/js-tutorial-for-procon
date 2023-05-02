1dda945e56714fe75126200185020305649b1b7e
tatata133 <taichengdaocun050@gmail.com>
2023-05-02 16:20:34 +0900

    放物線を描く小山高専

--- main.js
+++ main.js
@@ -8,10 +8,13 @@ GameArea.refresh();
 let oyamaLogo = new CanvasComponents({
   ctx: MainContext,
   img: "./assets/Oyama_logo.png",
-  position: new Vector2(GameArea.x / 2, GameArea.y / 2),
+  position: new Vector2(GameArea.x / 2, GameArea.y / 4),
 });
 oyamaLogo.update = function () {
-  this.rotate += 10;
+  this.position.x += 2;
+  this.position.y += 0;
+  this.motion.y += 0.3;
+  this.position.y += this.motion.y ;
 };
 
 function update() {
---
+++ testworks/main.js
@@ -0,0 +1,5 @@
+let a=15;
+let b=20;
+let c=10;
+let ans=(2*a/c)*b;
+console.log(ans);
