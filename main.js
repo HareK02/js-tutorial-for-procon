ea607ede12bfc29eafee19b3f30b48dfd4113b6e
Ganache773 <131744883+Ganache773@users.noreply.github.com>
2023-05-02 16:56:13 +0900

    test
    ＼(((￣(￣(￣▽￣)￣)￣)))／

--- main.js
+++ main.js
@@ -11,7 +11,9 @@ let oyamaLogo = new CanvasComponents({
   position: new Vector2(GameArea.x / 2, GameArea.y / 2),
 });
 oyamaLogo.update = function () {
-  this.rotate += 10;
+  this.rotate += 77;
+  this.position.x +=2
+  this.position.y -=1
 };
 
 function update() {
---
+++ testworks/main.js
@@ -0,0 +1,5 @@
+let a = 15;
+let b = 20;
+let c = 3
+let ans = a*2*b/c
+console.log(ans)
