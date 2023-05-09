247392262aac4ee66edc17723b4785d26ac9d54f
sone-taiki <101370571+sone-taiki@users.noreply.github.com>
2023-05-09 16:29:49 +0900

    課題#3

--- main.js
+++ main.js
@@ -13,20 +13,22 @@ let oyamaLogo = new CanvasComponents({
   position: new Vector2(GameArea.x / 2, 10),
 });
 oyamaLogo.update = function () {
-  this.motion.y += 1;
-  this.rotate += 10;
-  this.position.x += 5;
-  this.position.y += this.motion.y;
+  
+  if(keyInput.IsPressed("ArrowLeft")){
+    this.position.x -= 10;
+  }
+   if(keyInput.IsPressed("ArrowRight")){
+    this.position.x += 10;
+  }
+   if(keyInput.IsPressed("ArrowUp")){
+    this.position.y -= 10;
+  }
+   if(keyInput.IsPressed("ArrowDown")){
+    this.position.y += 10;
+  }
   
   position: new Vector2(GameArea.x / 2, GameArea.y / 4);
 };
-oyamaLogo.update = function () {
-  this.rotate -= 10;
-  this.position.x += 6;
-  this.motion.y += 1;
-  this.position.y += this.motion.y;
-};
-
 function update() {
   // your code goes here
 }
--- testworks/main.js
+++ testworks/main.js
@@ -1,6 +1,10 @@
-const a = 100;
-const b = 0.01;
-const c = 2;
-//let ans = a+b;
-console.log((2*a/c)*b);
-alert((2*a/c)*b);
+let a = 20;
+let b = 10;
+
+if(a > b){
+    console.log("a is bigger than b");
+}else if(a< b){
+    console.log("a is smaller than b");
+}else{
+    console.log("a is equal to b");
+}
