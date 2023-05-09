b8f89cc896493396e86c32a65eb3e51965205eb4
Hikooee34 <131746106+Hikooee34@users.noreply.github.com>
2023-05-09 16:30:38 +0900

    a

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const MainCanvas = document.getElementById("MainCanvas");
 const MainContext = MainCanvas.getContext("2d");
 const GameArea = new CanvasManager(new Vector2(1280, 720), MainCanvas);
+const keyInput = new keyInputManager();
 GameArea.refresh();
 
 //回り続ける小山高専
@@ -11,12 +12,19 @@ let oyamaLogo = new CanvasComponents({
   position: new Vector2(GameArea.x / 2, GameArea.y / 2),
 });
 oyamaLogo.update = function () {
-  this.rotate += 10;
-  this.position.x += 5;
-  this.position.y += 10;
-  this.motion.y += 1;
-  this.position.y += this.motion.y;
-};
+  if (keyInput.IsPressed("ArrowLeft")){
+    this.position.x -= 10;
+  }
+  if (keyInput.IsPressed("ArrowRight")){
+    this.position.x += 10;
+  }
+  if (keyInput.IsPressed("ArrowUp")){
+    this.position.y -= 10;
+  }
+  if (keyInput.IsPressed("ArrowDown")){
+    this.position.y += 10;
+  }
+}
 
 function update() {
   // your code goes here
