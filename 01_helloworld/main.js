40e97a39d8c9e4f4a3f9b4531d8df51957ad58d7
Pianissimo2007 <131745672+Pianissimo2007@users.noreply.github.com>
2023-05-09 15:58:18 +0900

    change filename

--- 01_helloworld/main.js
+++
@@ -1,5 +0,0 @@
-let a = 15;
-let b = 20;
-let c = 30;
-let ans = (2*a/c)*b;
-console.log(ans);
---
+++ testworks/main.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 20;
+if (a>b) {
+    console.log("a is bigger than b");
+} else if (a<b) {
+    console.log("a is smoller than b");
+} else{
+    console.log("a is equal to b");
+}
