1194bfcf3cb71e5404eb940b1c5c33c0d1df4058
Warapikom <131745458+Warapikom@users.noreply.github.com>
2023-05-12 16:42:43 +0900

    増殖

--- testworks/main.js
+++ testworks/main.js
@@ -1,9 +1,21 @@
-let a = 10;
-let b = 20;
-if(a > b){
-    console.log("a is bigger than b");
-}else if(a < b){
-    console.log("a is smaller than b");
-}else{
-    console.log("a is equal to b");
+let arrey = ["ジョン","マイケル","ダニエル","ボブ"];
+console.log(arrey[2]);
+arrey[arrey.length] = "ティム";
+console.log(arrey);
+
+let obj = {name:"ジョン" ,age:15};
+console.log(obj.name);
+console.log(obj["age"]);
+console.log(obj);
+
+let arr  = [];
+for (let i = 0; i < 9; i++) {
+    arr[i] = [];
+    for (let k = 0; k < 9; k++) {
+        arr[i][k] = (i + 1) * (k + 1);
+    }
 }
+console.log(arr[5][1]); //6*2=12
+console.log(arr[2][6]); //3*7=21
+console.log(arr[6][8]); //7*9=63
+console.log(arr);
