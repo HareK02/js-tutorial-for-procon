7c8955463c755b64cf396a6cc210ca7dbc45d4fa
matsumaruhiroki <hiro.16340101@gmail.com>
2023-05-19 14:46:56 +0900

    課題

--- testworks/main.js
+++ testworks/main.js
@@ -1,9 +1,11 @@
-let a = 60000;
-let b = 60000;
-if (a > b) {
-  console.log("a is bigger than b");    
-} else if (a < b) {
-  console.log("a is smaller than b");    
-} else {
-  console.log("a is equal to b");    
+let arr = [];
+for (let i = 0; i < 9; i++) {
+  arr[i] = [];
+  for (let k = 0; k < 9; k++) {
+    arr[i][k] = (i + 1) * (k + 1);
+  }
 }
+console.log(arr[5][1]); //6*2=12
+console.log(arr[2][6]); //3*7=21
+console.log(arr[6][8]); //7*9=63
+console.log(arr);
